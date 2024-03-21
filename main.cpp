3c4884ea985968354ef083b1cb5907bb3de7ec3d
mustafa389 <mustafaasghar66@gmail.com>
2024-03-21 15:16:08 +0500

    asci values are correct

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+
+int main(){
+    
+    // Open the input file
+    std::ifstream inputFile("letters_100.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening the file." << std::endl;
+        return 1; // Exit with error
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        // Print the original line
+        std::cout << "Line: " << line << std::endl;
+
+        // Tokenize the line into words
+        std::istringstream iss(line);
+        
+        std::string word;
+        while (iss >> word) {
+            std::cout<<"the word is: "<<word<<std::endl;
+            int word_asci=0;
+            for (char c : word) {
+                // std::cout << c << " ";
+                int asciiValue = static_cast<int>(c);
+                word_asci+=asciiValue;
+
+
+            }
+            std::cout<<"the asci value of the word is: "<<word_asci<<std::endl;
+        }
+
+        // Print each word
+        std::cout << "Words: ";
+        
+    }
+
+    // Close the file
+    inputFile.close();
+
+
+    return 0;
+}
