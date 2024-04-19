d7955f7443a0250f1b1c2829bc03ba80a388db76
mustafa389 <mustafaasghar66@gmail.com>
2024-04-19 16:35:54 +0500

    did some testing

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main(){
         }
 
         // Print each word
-        std::cout << "Words: ";
+        // std::cout << "Words: ";
         
     }
 
@@ -71,8 +71,10 @@ int main(){
     //this search function looks for the node that has the aci value as that of its parameter and then returns that node when it finds it
     Node* returned_node=tree.search("watching"); //disordinance Magnetizable have asci values 1235
     tree.print_words_on_node(returned_node); //this prints all the words on a node
+    tree.deleteWord("watching");
+    tree.print_words_on_node(returned_node); //this prints all the words on a node
+
     // tree.print_words_on_node(returned_node);
-    // tree.deleteWord("Sumner");
 
 
     
